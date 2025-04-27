9246d73a44ff47181282cf63b7ee3ed0ee3c456b
SharkSpidy <josephshibuwork@gmail.com>
2025-04-27 19:16:57 +0530

    Added Image

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ detector = HandDetector(detectionCon=0.8)
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
+    img1 = cv2.imread('prof1.png')
+
+    img[0:500, 0:500] = img1
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord('q'):
