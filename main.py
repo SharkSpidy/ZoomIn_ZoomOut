0fc551775da01dc9f64abc24093a7ec3b41cab53
SharkSpidy <josephshibuwork@gmail.com>
2025-04-27 20:03:22 +0530

    Distance Measurement

--- main.py
+++ main.py
@@ -2,25 +2,37 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
+cap.set(3, 1080)
 cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
-
+startDist = None
 
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
-    img1 = cv2.imread('prof1.png')
+    img1 = cv2.imread('1.png')
 
 
     if len(hands) == 2:
         if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
-            print("Zoom")
+            lmList1 = hands[0]["lmList"]
+            lmList2 = hands[1]["lmList"]
+            
+            if startDist is None:
+                length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+                startDist = length
+            
+            length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
+            scale = length - startDist
+            
+            print(scale)
+    else:
+        startDist = None
 
 
 
-    img[0:500, 0:500] = img1
+    img[10:260, 10:260] = img1
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord('q'):
